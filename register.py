dbcd219b7fd763954327127aca6a5f23c56abdd4
unknown <sacali99@gmail.com>
2020-10-24 18:53:29 -0500

    conexion con la bd y agregación de los datos del usuario

---
+++ register.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import mysql.connector
+from mysql.connector import errorcode
+import cgi
+
+data = cgi.FieldStorage()
+
+data = cgi.FieldStorage()
+nombre = data.getvalue('nom')
+email = data.getvalue('correo')
+password = data.getvalue('con')
+num_cel = data.getvalue('ph')
+dire = data.getvalue('add')
+
+try:
+    cnx = mysql.connector.connect(
+        user='Caceres', password='s28Nor04+', database='baseProyecto', host='127.0.0.1')
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cur = cnx.cursor()
+    print('Content-Type: text/html')
+    print('')
+    sql = ("insert into Usuario values ('{}','{}',SHA( '{}'),'{}','{}')".format(nombre, email, password, num_cel, dire))
+    cur.execute(sql)
+    cnx.commit()
+    print('<script> location.href="/Entregable2CorteSoftware/index.html";</script>')
+cnx.close()
