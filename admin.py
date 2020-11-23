84a30ad7a4d90159984bf9386fd840688846ac8a
unknown <sacali99@gmail.com>
2020-11-23 16:13:39 -0500

    .

--- admin.py
+++ admin.py
@@ -47,10 +47,7 @@ else:
             'Telefono': tel,
             'Dirección': dire, 
         }
-        print (datos)
         datos_json = json.dumps(datos)
-        print('datos JSON',datos_json)
-        type(datos_json)
         '''infoBD(nom,email,contra,tel,dire)'''
 
 cnx.close()
