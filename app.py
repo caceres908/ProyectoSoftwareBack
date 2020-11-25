0f84a9d2b94d8f9305a61ebd5e04a721605a13ad
unknown <sacali99@gmail.com>
2020-11-25 16:57:51 -0500

    cambio ruta

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+import mysql.connector
+from mysql.connector import errorcode
 from flask import Flask 
+from flask_cors import CORS
+import json
 
+# configuration
+DEBUG = True
+
+# instantiate the app
 app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
 
 @app.route('/')
 def hellos():
@@ -8,7 +22,48 @@ def hellos():
 
 @app.route('/productosM', methods=['GET'])
 def mostrar_productos():
-	return "Mostrando productos owo";
+	print('Content-Type: text/json')
+print('')
+
+try:
+    cnx = mysql.connector.connect(
+        user='Caceres', password='s28Nor04+', database='baseProyecto', host='127.0.0.1')
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cur = cnx.cursor()
+    sql = ("select* from Usuario ")
+    cur.execute(sql)
+    row = cur.fetchall()
+    print('[')
+    lon = len(row)
+    j = 1
+    for i in row:
+        nom = i[0]
+        email = i[1]
+        contra = i[2]
+        tel = i[3]
+        dire = i[4]
+        datos = {
+            'Nombre': nom,
+            'Email': email,
+            'password': contra,
+            'Telefono': tel,
+            'Direccion': dire,
+        }
+        datos_json = json.dumps(datos)
+        print(datos_json)
+        if j < lon:
+          print(',')
+          j=j+1
+        '''infoBD(nom,email,contra,tel,dire)'''
+    print(']')
+cnx.close()
 
 @app.route('/productosMO', methods=['PUT'])
 def modificar_productos():
