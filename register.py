5cec30d22b274e556af748beb770aac4bc472ae2
unknown <sacali99@gmail.com>
2020-12-02 00:22:39 -0500

    agrego comentarios al codigo

--- register.py
+++ register.py
@@ -25,7 +25,7 @@ else:
     cur = cnx.cursor()
     print('Content-Type: text/html')
     print('')
-    sql = ("insert into Usuario values ('{}','{}',SHA( '{}'),'{}','{}')".format(nombre, email, password, num_cel, dire))
+    sql = ("insert into Usuario (nombre,Email,contraseña,numero,direccion) values ('{}','{}',SHA( '{}'),'{}','{}')".format(nombre, email, password, num_cel, dire))
     cur.execute(sql)
     cnx.commit()
     print('<script> location.href="/Entregable2CorteSoftware/index.html";</script>')
